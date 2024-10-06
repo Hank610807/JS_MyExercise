75f4e270df62e09639f50b599740bddfe7529d14
Hank.weng <hank610807@gmail.com>
2024-10-06 22:10:58 +0800

    first commit

---
+++ 20241006/script.js
@@ -0,0 +1,15 @@
+a = 100;
+b = 100;
+
+console.log(a + b)
+console.error(a + b)
+console.warn(a + b)
+console.info(a + b)
+
+console.clear()
+
+c = 'hello';
+d = 'world';
+console.log('hello world');
+console.log(c + ' ' + d);
+console.log(`${c} ${d}`);
